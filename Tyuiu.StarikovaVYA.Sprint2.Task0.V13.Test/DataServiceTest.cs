3db1fdc717e1326f32ab2a39cbc04cdac4de76a1
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-23 01:22:01 +0500

    Task0

--- Tyuiu.StarikovaVYA.Sprint2.Task0.V13.Test/DataServiceTest.cs
+++ Tyuiu.StarikovaVYA.Sprint2.Task0.V13.Test/DataServiceTest.cs
@@ -8,7 +8,12 @@ namespace Tyuiu.StarikovaVYA.Sprint2.Task0.V13.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-
+            int x = 1905;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
