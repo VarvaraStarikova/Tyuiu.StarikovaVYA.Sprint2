f957f5e5785d62dd1ea8e8811bd28ac695a9a21d
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-23 21:22:54 +0500

    Task1

---
+++ Tyuiu.StarikovaVYA.Sprint2.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.StarikovaVYA.Sprint2.Task1.V2.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint2.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int a = 135;
+            int b = 156;
+            int c = 154;
+            int d = 174;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, true, true, true, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint2.Task1.V2/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.StarikovaVYA.Sprint2.Task1.V2.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint2.Task1.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 135;
+            int b = 156;
+            int c = 154;
+            int d = 174;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+
+            Console.Title = "Спринт #2 | Выполнила: Старикова В. Я. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Старикова Варвара Яковлевна | СМАРТб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+            Console.WriteLine("* которая вернет логическую последовательность                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
