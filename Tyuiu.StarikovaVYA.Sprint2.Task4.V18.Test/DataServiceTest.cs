32a20dcb74d7a76b9130bc809ded7c5fa5637436
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-24 05:28:55 +0500

    Task4

---
+++ Tyuiu.StarikovaVYA.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.StarikovaVYA.Sprint2.Task4.V19.Lib;
+
+namespace Tyuiu.StarikovaVYA.Sprint2.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 8;
+            double res = ds.Calculate(x, y);
+            double wait = 36.754;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
