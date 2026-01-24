01cf7c8af0e25d33804ee36585f70a4cdd7bb283
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-24 06:18:56 +0500

    Task5

---
+++ Tyuiu.StarikovaVYA.Sprint2.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.StarikovaVYA.Sprint2.Task5.V14.Lib
+{
+    public class DataService : ISprint2Task5V14
+    {
+        public string FindDayName(int k, int d)
+        {
+            string res;
+            int o = (k - 1) % 7;
+            int h = (d + o - 1) % 7 + 1;
+            switch (h)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"День недели должен быть от 1 до 7. Значение {d}");
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint2.Task5.V14/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.StarikovaVYA.Sprint2.Task5.V14.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint2.Task5.V14
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Старикова В. Я. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание 5                                                               *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Старикова Варвара Яковлевна | СМАРТб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Дано целое число k. Определить, каким днем недели является k-й день     *");
+            Console.WriteLine("* не високосного года, в котором 1 января d-й день недели.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной d: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if (k < 1 | k > 365 | d < 1 | d > 7)
+            {
+                res = "Введено неверное значение";
+            }
+            else
+            {
+                res = "День недели: " + ds.FindDayName(k, d);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
