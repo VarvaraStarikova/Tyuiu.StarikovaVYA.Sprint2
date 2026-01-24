e123eb98508d59a57ae2f206fafc39abaa88bc3a
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-24 06:53:46 +0500

    Task6

---
+++ Tyuiu.StarikovaVYA.Sprint2.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.StarikovaVYA.Sprint2.Task6.V13.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint2.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+
+            int g = 2020;
+            int m = 2;
+            int n = 28;
+            var res = ds.FindDateOfNextDay(g, m, n);
+            double wait = 20200229;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
